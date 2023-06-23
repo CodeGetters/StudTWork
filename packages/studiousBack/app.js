1e849956cdf2ebf5427c0d297d771c5b485e698b
Johnson Xin <2775145275@qq.com>
2023-06-23 19:45:36 +0800

    feat: successfully sent the request to the back-end

--- packages/studiousBack/app.js
+++ packages/studiousBack/app.js
@@ -4,16 +4,42 @@
  * @version:
  * @Date: 2023-06-18 20:30:52
  * @LastEditors: CodeGetters
- * @LastEditTime: 2023-06-21 16:43:43
+ * @LastEditTime: 2023-06-23 19:12:33
  */
 const koa = require("koa");
 const cors = require("koa-cors");
 
+// https://github.com/koajs/convert/blob/master/README.md
+// https://github.com/koajs/convert#migration
+// const convert = require("koa-convert");
+
 const app = new koa();
 
-app.use(cors);
-app.use(async (ctx) => {
-  ctx.body = "hello world";
+const Router = require("./router/index");
+
+app.use(cors());
+
+// X-Response-Time
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set("X-Response-Time", `${ms}ms`);
 });
-console.log("http://127.0.0.1:5000");
+
+// logger
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+
+// response
+app.use(async (ctx) => {
+  ctx.body = "hello woeld";
+});
+
+app.use(Router.routes(), Router.allowedMethods());
+
 app.listen(5000);
--- packages/studiousFront/src/api/index.js
+++ packages/studiousFront/src/api/index.js
@@ -4,18 +4,38 @@
  * @version:
  * @Date: 2023-06-21 14:39:04
  * @LastEditors: CodeGetters
- * @LastEditTime: 2023-06-21 16:35:13
+ * @LastEditTime: 2023-06-23 17:00:57
  */
 import axios from "axios";
 
 const service = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
-  headers: {},
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+  },
 });
 
 // 请求拦截器
-service.interceptors.request.use();
+service.interceptors.request.use(
+  function (config) {
+    // 在发送请求之前做什么
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
 
 // 响应拦截器
-service.interceptors.response.use();
+service.interceptors.response.use(
+  function (config) {
+    // 2xx 范围内的状态码都会触发该函数
+    // 对响应做什么
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
 export default service;
